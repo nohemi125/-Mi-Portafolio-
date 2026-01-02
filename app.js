91bd3f937fd2c65b5220b9e8c9b96ee904eca190
nohemy morelo <nohemy_morelo69222@elpoli.edu.co>
2026-01-01 21:38:50 -0500

    cambio de var y puerto

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error(err));
 
 /* servidor */
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Servidor corriendo en el puerto', PORT);
 });
